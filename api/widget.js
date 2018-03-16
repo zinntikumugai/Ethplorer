426514d89110c7dfc04cee089831a24049075f0e
Lexa M <lexa-m@users.noreply.github.com>
2018-03-16 19:50:50 +0700

    Period param added.

--- api/widget.js
+++ api/widget.js
@@ -149,7 +149,8 @@ ethplorerWidget = {
             }
             var params = {
                 apiKey: 'ethplorer.widget',
-                domain: document.location.href
+                domain: document.location.href,
+                period: 730
             };
 
             $.getJSON(api, params, function(_address){
