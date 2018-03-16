662f5fcd954987e946ee95ed82f99c3f6f881ea6
Lexa M <lexa-m@users.noreply.github.com>
2018-03-16 19:31:56 +0700

    Min for price history period changed.

--- api/controller.php
+++ api/controller.php
@@ -447,7 +447,7 @@ class ethplorerController {
      * @return array
      */
     public function getTokenPriceHistoryGrouped(){
-        $period = min(abs((int)$this->getRequest('period', 365)), 365);
+        $period = abs((int)$this->getRequest('period', 365));
         if($period <= 0) $period = 365;
         $address = $this->getParam(0, FALSE);
         if($address){
