01feadad3f9f06992167ee08a3418532184117da
Lexa M <lexa-m@users.noreply.github.com>
2018-04-10 20:18:43 +0700

    Redis support.

--- service/lib/cache.php
+++ service/lib/cache.php
@@ -98,6 +98,8 @@ class evxCache {
         }
         $this->useLocks = $useLocks;
 
+        if(class_exists('Redis') && (php_sapi_name() === 'cli') && isset($aConfig['redis'])) $this->driver = 'redis';
+
         if('memcached' === $this->driver){
             if(class_exists('Memcached')){
                 $mc = new Memcached('ethplorer');
