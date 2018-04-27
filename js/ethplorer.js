2be656ee31a430ae03495b49dfe5a94e4a4766ec
Artem <artemk@211.ru>
2018-04-27 18:32:02 +0700

    Regexp fixed

--- js/ethplorer.js
+++ js/ethplorer.js
@@ -2140,7 +2140,7 @@ Ethplorer = {
             }
             var fullStr = Ethplorer.Utils.hex2ascii(hex);
             if(!res && fullStr.indexOf("Issuance")){
-                res = {description: fullStr.substr(fullStr.indexOf("Issuance")).replace("\\n", "\n").replace("\\u0000", "")};
+                res = {description: fullStr.substr(fullStr.indexOf("Issuance")).replace(/\\n/g, "\n").replace(/\\u0000/g, "")};
             }
             return res;
         },
