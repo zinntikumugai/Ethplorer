f6ebaba8cf6c0cfcb90073f869db5fa0dbcd42c5
Lexa M <lexa-m@users.noreply.github.com>
2018-06-22 20:27:53 +0700

    Increase code version.

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $aConfig = require dirname(__FILE__) . '/service/config.php';
 require dirname(__FILE__) . '/service/lib/ethplorer.php';
 $es = Ethplorer::db(array());
 
-$codeVersion = isset($aConfig['codeVersion']) ? $aConfig['codeVersion'] : "213";
+$codeVersion = isset($aConfig['codeVersion']) ? $aConfig['codeVersion'] : "214";
 
 $error = TRUE;
 $header = "";
