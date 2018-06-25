2ba17bb722d2f79f6c52d509703b8ded7dd4056a
Lexa M <lexa-m@users.noreply.github.com>
2018-06-25 17:35:39 +0700

    Allow eth transfers in address widget.

--- api/controller.php
+++ api/controller.php
@@ -507,7 +507,7 @@ class ethplorerController {
                 $this->sendError(104, 'Invalid address format');
             }
         }
-        $withEth = !!$this->getRequest('withEth', FALSE);
+        $withEth = $this->getRequest('withEth', FALSE);
         $result = array('history' => $this->db->getAddressPriceHistoryGrouped($address, FALSE, $withEth));
         if(isset($result['history']['cache'])) $this->cacheState = $result['history']['cache'];
         else $this->cacheState = '';
