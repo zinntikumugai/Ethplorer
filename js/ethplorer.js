8545c124323d8fa6caf360a54fd195767b092c0f
Artem <artemk@211.ru>
2018-06-29 18:44:17 +0700

    dot fix

--- js/ethplorer.js
+++ js/ethplorer.js
@@ -2085,7 +2085,7 @@ Ethplorer = {
             if((num.toString().indexOf("e-") > 0) && withDecimals){
                 var res = Ethplorer.Utils.toBig(num).toFixed(decimals);
                 if(cutZeroes){
-                    res = res.replace(/0*$/, '');
+                    res = res.replace(/0*$/, '').replace(/\.$/, '');
                 }
                 return res;
             }
